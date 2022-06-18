5c1cd80fc6ee8434fca546b1d1ca791f3a97af3c
«Udodov» <k.udodov@internet.ru>
2022-06-18 14:10:33 +0300

    added a request to enter the size of a one-dimensional array and declared two one-dimensional arrays with the specified size

--- Control.cs
+++ Control.cs
@@ -1,1 +1,8 @@
-﻿
+﻿int n;
+//  Entering the array size and checking the input
+Console.Write("Enter the number of items you want to enter: ");
+while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)  // We read a string and convert it to a number. With a check for entering a positive integer
+    Console.WriteLine("The number cannot be recognized, enter a positive integer");
+
+string[] initialArray = new string[n]; // Declaration of one-dimensional arrays of strings of size n entered by the user
+string[] newArray = new string[n];
