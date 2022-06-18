18a6a70e7f76898ad12404fc55732820f6b75d8c
«Udodov» <k.udodov@internet.ru>
2022-06-18 13:44:36 +0300

    two one-dimensional arrays are declared, and the initial array is set

--- Control.cs
+++ Control.cs
@@ -1,1 +1,4 @@
-﻿
+﻿//  Setting the initial one-dimensional array
+string[] initialArray = { "1234", "1567", "-2", "computer science" };
+//  Declaring a new array with the size of the original array
+string[] newArray = new string[initialArray.Length];
