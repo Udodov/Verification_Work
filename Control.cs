40ae9f3b9cc52214432e87f3912085da63c92588
«Udodov» <k.udodov@internet.ru>
2022-06-18 13:45:30 +0300

    creating the PrintArray method

--- Control.cs
+++ Control.cs
@@ -2,3 +2,16 @@
 string[] initialArray = { "1234", "1567", "-2", "computer science" };
 //  Declaring a new array with the size of the original array
 string[] newArray = new string[initialArray.Length];
+
+Console.WriteLine("Initial array:");
+PrintArray(initialArray);
+
+void PrintArray(string[] arr)   // Output an array to the console
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+        if (i < arr.Length - 1)
+            Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i] + "]");
+    Console.WriteLine();
+}
